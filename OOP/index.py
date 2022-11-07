8e893d8c7ba208880b45358ee0ea778a1f3e6175
Erotomechanics <117106177+Erotomechanics@users.noreply.github.com>
2022-11-08 00:33:05 +0300

    Задание по MySQL

---
+++ OOP/index.py
@@ -0,0 +1,9 @@
+import mysql.connector
+mydb = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    passwd='1234',
+    database='my_base'
+)
+mycursor = mydb.cursor()
+
--- OOP/main.py
+++ OOP/main.py
@@ -1,10 +1,10 @@
+from index import mycursor, mydb
 class Person:
-    def __init__(self, name, age, profession, id):
+    def __init__(self, name, age, profession, blood_type):
         self.name = name
         self.age = age
         self.profession = profession
-        self.id = id
-        self.__kgb = 5
+        self.blood_type = blood_type
 
     @property
     def name(self):
@@ -31,21 +31,37 @@ class Person:
         self._profession = profession
 
     @property
-    def id(self):
-        return self._id
+    def blood_type(self):
+        return self._blood_type
 
-    @id.setter
-    def id(self, id):
-        self._id = id
+    @blood_type.setter
+    def blood_type(self, blood_type):
+        self._blood_type = blood_type
 
     def add_age(self):
         self.age += 5
 
 
-class Doctor(Person):
-    def __init__(self, name, age, id, profession):
-        super().__init__(name, age, id, profession)
-        self.pacients = []
-        self.number_of_operations = 0
-print()
-sfdfghf
+user_input = str()
+while True:
+    print('1.) Вывод данных', '2.) Ввод данных', '3.) Выход',sep='\n')
+    user_input = input()
+    if user_input == '2':
+        person = Person(*input('Введите данные о человеке ').split())
+        person_data = (person.name, person.age, person.profession, person.blood_type)
+        sqlFormula = 'INSERT person (name, age, profession, blood_type) VALUES (%s, %s, %s, %s)'
+        mycursor.execute(sqlFormula, person_data)
+        mydb.commit()
+        continue
+    elif user_input == '1':
+        mycursor.execute('SELECT * FROM person')
+        myresult = mycursor.fetchall()
+        for row in myresult:
+            print(row)
+        input()
+        continue
+    elif user_input == '3':
+        break
+    else:
+        print('Ошибка ввода, попробуйте ещё раз')
+        continue
